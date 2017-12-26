d154d50c0b85d8411e9d54ba132f4fa843952dc7
Chu2015 <1198209488@qq.com>
2017-12-26 23:54:59 +0800

    update

--- abaqus_result/dis_force.m
+++ abaqus_result/dis_force.m
@@ -4,31 +4,29 @@ figure(1);
 
 %  a = plot(u_60_0_lin,f_60_0_lin,'r');hold on;
 %  b = plot(u_60_0_exp,f_60_0_exp,'r');hold on;
-  c = plot(u_60_45_lin,f_60_45_lin,'r','LineWidth',3);hold on;
-  d = plot(u_60_45_exp,f_60_45_exp,':r','LineWidth',3);hold on;
-% e = plot(u_60_0_90_lin,f_60_0_90_lin,'r');hold on;
-% f = plot(u_60_0_90_exp2,f_60_0_90_exp2,'b');hold on;
-
-% legend([a,b,c,d,e,f],'60-0-lin','60-0-exp','60-45-lin','60-45-exp','60-0-90-lin','60-0-90-exp');
-% grid on
-% xlabel('Œª“∆(mm)');
-% ylabel('‘ÿ∫…(N)');
-% axis([0 4 0 140000]);
+%  c = plot(u_60_45_lin,f_60_45_lin,'r','LineWidth',3);hold on;
+%  d = plot(u_60_45_exp,f_60_45_exp,':r','LineWidth',3);hold on;
+  e = plot(u_60_0_90_lin,f_60_0_90_lin,'r','LineWidth',3);hold on;
+  f = plot(u_60_0_90_exp,f_60_0_90_exp,':r','LineWidth',3);hold on;
 
 %  aa = plot(u_80_0_lin,f_80_0_lin,'g');hold on;
 %  bb = plot(u_80_0_exp,f_80_0_exp,'g');hold on;
-   cc = plot(u_80_45_lin,f_80_45_lin,'b','LineWidth',3);hold on;
-   dd = plot(u_80_45_exp,f_80_45_exp,':b','LineWidth',3);hold on;
+%  cc = plot(u_80_45_lin,f_80_45_lin,'b','LineWidth',3);hold on;
+%  dd = plot(u_80_45_exp,f_80_45_exp,':b','LineWidth',3);hold on;
+   ee = plot(u_80_0_90_lin,f_80_0_90_lin,'b','LineWidth',3);hold on;
+   ff = plot(u_80_0_90_exp,f_80_0_90_exp,':b','LineWidth',3);hold on;
    
 %  aaa = plot(u_100_0_lin,f_100_0_lin,'b');hold on;
 %  bbb = plot(u_100_0_exp,f_100_0_exp,'b');hold on;
 %  aaaa = plot(u_100_0_lin_2_5,f_100_0_lin_2_5,'r');hold on;
-   ccc = plot(u_100_45_lin,f_100_45_lin,'k','LineWidth',3);hold on;
-   ddd = plot(u_100_45_exp,f_100_45_exp,':k','LineWidth',3);hold on;
+%  ccc = plot(u_100_45_lin,f_100_45_lin,'k','LineWidth',3);hold on;
+%  ddd = plot(u_100_45_exp,f_100_45_exp,':k','LineWidth',3);hold on;
+   eee = plot(u_100_0_90_lin,f_100_0_90_lin,'k','LineWidth',3);hold on;
+   fff = plot(u_100_0_90_exp,f_100_0_90_exp,':k','LineWidth',3);hold on;
    
 %  legend([a,b,c,d,aa,bb,aaa,bbb,aaaa],'60-0-lin','60-0-exp','60-45-lin','60-45-exp','80-0-lin','80-0-exp','100-0-lin','100-0-exp','100-0-lin-2.5');
- legend([c,d,cc,dd,ccc,ddd],'60-45-lin','60-45-exp','80-45-lin','80-45-exp','100-45-lin','100-45-exp');
+ legend([e,f,ee,fff,eee,fff],'60-45-lin','60-45-exp','80-45-lin','80-45-exp','100-45-lin','100-45-exp');
  grid on
  xlabel('Œª“∆(mm)');
  ylabel('‘ÿ∫…(N)');
- axis([0 4 0 14000]);
+ axis([0 4 0 55000]);
